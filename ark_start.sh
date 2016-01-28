2df6ef37d3b1cbca726a20557261c72fc4d9331b
Fabien Foerster <fabienfoerster@gmail.com>
2016-01-28 14:51:36 +0100

    add env var for updating server

--- ark_start.sh
+++ ark_start.sh
@@ -3,7 +3,7 @@
 # if not empty install the game server
 if [ ! "$(ls -A ${SERVERPATH})" ]
 then
-  echo -e "Installing ARK  in ${SERVERPATH}... \n"
+  echo -e "Installing Ark server ... \n"
   /steamcmd/steamcmd.sh +login anonymous +force_install_dir ${SERVERPATH} +app_update 376030 +quit
 
   #copy the GameUserSettings.ini file
@@ -13,8 +13,14 @@ then
 fi
 
 
+if [ "$UPDATE_SERVER" = "true"]
+then
+  echo -e "Updating Ark server ...."
+  /steamcmd/steamcmd.sh +login anonymous +force_install_dir ${SERVERPATH} +app_update 376030 +quit
+fi
+
 
 
 #Lauch the game server
-echo -e "Lauching Ark Server"
+echo -e "Lauching Ark Server ...."
 ${SERVERPATH}/ShooterGame/Binaries/Linux/ShooterGameServer TheIsland?listen?SessionName=${ARK_SERVER_NAME:=[FR]Brookie-Ark}?ServerPassword=${ARK_SERVER_PASSWORD:=ark}?ServerAdminPassword=${ARK_SERVER_ADMIN_PASSWORD:=admin}?Port=${ARK_SERVER_PORT:=7777}?QueryPort=${ARK_SERVER_QUERYPORT:=27015}?allowThirdPersonPlayer=${ARK_BOOLEAN:=true}?alwaysNotifyPlayerLeft=${ARK_BOOLEAN:=true}?alwaysNotifyPlayerJoined=${ARK_BOOLEAN:=true}?serverPVE=${ARK_BOOLEAN:=true}?MaxPlayers=${ARK_MAX_PLAYER:=10} -nosteamclient -game -server -log
